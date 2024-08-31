09be3a2f628e12a2f9a5538f9d43c5b054e5fa4a
Alec Mathisen <Alec-WAM@users.noreply.github.com>
2024-08-31 14:21:58 -0400

    Initial Commit

---
+++ app/src/main/java/alec_wam/musicplayer/ui/playlists/PlaylistsFragment.java
@@ -0,0 +1,36 @@
+package alec_wam.musicplayer.ui.playlists;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import alec_wam.musicplayer.databinding.FragmentPlaylistsBinding;
+
+public class PlaylistsFragment extends Fragment {
+
+    private FragmentPlaylistsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        PlaylistsViewModel playlistsViewModel =
+                new ViewModelProvider(this).get(PlaylistsViewModel.class);
+
+        binding = FragmentPlaylistsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textPlaylists;
+        playlistsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
